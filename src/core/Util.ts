733f6eb0202d36c8de85b3781d2f5c07e9edfb98
terza <lukaterzicbg@gmail.com>
2024-09-18 13:17:39 +0200

    added si formatting for numbers to util

--- src/core/Util.ts
+++ src/core/Util.ts
@@ -2,3 +2,16 @@ export function objectMap<T, U>(obj: T, u: (k: string, v: T[keyof T]) => U) {
     // @ts-expect-error
     return Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, u(k, v)]));
 }
+
+const magnitudeUnits = [
+    'p', 'n', '&micro;', 'm', '', 'k', 'M', 'G', 'T'
+];
+
+export function numberSIFormat(x: number): string {
+    const exp = Math.log10(Math.abs(x));
+    if (exp < -15)
+        return "0";
+
+    const exp1000 = Math.max(-4, Math.min(4, Math.floor(exp / 3)));
+    return (x * Math.pow(1000, -exp1000)).toPrecision(3) + magnitudeUnits[exp1000 + 4];
+}
